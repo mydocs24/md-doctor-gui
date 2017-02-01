26925a3b2e4c2c6f482669b1c599ffef6e37a0eb
Alexander Zagovorichev <zagovorichev@gmail.com>
2017-02-01 17:50:24 +0300

    100% unit tests

--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="entrance">
-    <input type="text" class="login" :value="login">
-    <input type="password" class="password" :value="password">
+    <input type="text" class="login" :value="login" :placeholder="$t('auth.login')">
+    <input type="password" class="password" :value="password" :placeholder="$t('auth.password')">
   </div>
 </template>
 
 <script>
 export default {
-  name: 'login',
+  name: 'authenticate',
   data () {
     return {
       login: '',
