19e40f3cc16fce866a3de9eddb1e212812b182a8
zagovorichev <zagovorichev@gmail.com>
2017-03-05 21:51:09 +0300

    New design

--- src/controllers/Dashboard.vue
+++ src/controllers/Dashboard.vue
@@ -1,8 +1,30 @@
 <template>
     <div class="row">
-        <div class="col-sm-9">
-            <h1>My Cases</h1>
+        <div class="col-sm-2">
+            doc info
+            For example:
+            Photo
+            Name of Doctor
+            City: Tenerife
+            Closed Cases for season 2017: 10
+        </div>
+        <div class="col-sm-7">
             <cases-table></cases-table>
+        </div>
+        <div class="col-sm-3">
+            <b-alert :show="true" state="success" dismissible>
+                  This is an alert
+            </b-alert>
+            messages,another info
+            For example:
+            Your case was approved and closed
+            Your case was payed
+
+            <div class="card">
+                <div class="card-block">
+                    @ 2017 MyDoctor
+                </div>
+            </div>
         </div>
     </div>
 </template>
