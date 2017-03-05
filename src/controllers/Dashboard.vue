d316b8a7dbb5a4a62e1be3a60dd9f3c2153697a7
MedCenter24 Dev <dev@medcenter24.com>
2017-03-05 14:13:39 +0300

    bootstrap datatable features

---
+++ src/controllers/Dashboard.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="row">
+        <div class="col-sm-9">
+            <h1>My Cases</h1>
+            <cases-table></cases-table>
+        </div>
+    </div>
+</template>
+<style>
+</style>
+<script>
+import CasesTable from './cases/CasesTable'
+
+export default {
+
+  data () {
+    return {
+      countActive: 10
+    }
+  },
+  components: {
+    CasesTable
+  }
+}
+
+</script>
