9afe000e2317f47bcb76ddc732e0a1741d625bf5
zagovorichev <zagovorichev@gmail.com>
2017-03-06 00:22:13 +0300

    New design

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,31 @@
 <template>
-  <div id="app" class="container">
+  <div id="app">
 
-    <div>
+    <b-navbar toggleable type="inverse" variant="mydoc">
+
+      <b-nav-toggle target="nav_collapse"/>
+
       <router-link to="/">Main</router-link>
-      <router-link to="/component1">Component 1</router-link>
-      <router-link to="/component2">Component 2</router-link>
-      <router-link to="/hello">Hello</router-link>
-    </div>
 
-    <router-view></router-view>
+      <b-collapse isNav id="nav_collapse">
+        <b-nav isNavBar>
+          <b-nav-item>
+            <router-link to="/component1">Component 1</router-link>
+          </b-nav-item>
+          <b-nav-item>
+            <router-link to="/component2">Component 2</router-link>
+          </b-nav-item>
+          <b-nav-item>
+            <router-link to="/hello">Hello</router-link>
+          </b-nav-item>
+        </b-nav>
+      </b-collapse>
+
+    </b-navbar>
+
+    <div class="container main">
+      <router-view></router-view>
+    </div>
 
   </div>
 </template>
@@ -17,4 +34,12 @@
 @import '../node_modules/bootstrap/scss/bootstrap';
 @import "sass/variables";
 @import "sass/style";
+
+  .bg-mydoc {
+    background-color: #3097D1;
+  }
+
+  .main {
+    margin-top: $offset-bottom;
+  }
 </style>
--- src/controllers/Dashboard.vue
+++ src/controllers/Dashboard.vue
@@ -1,18 +1,33 @@
 <template>
     <div class="row">
         <div class="col-sm-2">
-            doc info
-            For example:
-            Photo
-            Name of Doctor
-            City: Tenerife
-            Closed Cases for season 2017: 10
+            <div class="card doctor-info">
+                <img class="card-img-top"
+                     src="https://scontent.fhen1-1.fna.fbcdn.net/v/t1.0-9/13007374_971838689558988_8853583067139950859_n.jpg?oh=1ac22b8094c9a93c13c4692c3fa809d0&oe=59300A59"
+                     height="100px"
+                     alt="doctor.name">
+                <div class="card-block">
+                    <h6>Alexander Zagovorichev</h6>
+                </div>
+            </div>
+
+            <div class="card card-about">
+                <div class="card-block">
+                    <h6>About
+                        <small> · <a>Edit</a></small></h6>
+
+                    <b>City</b>
+                    <br><span class="val">Tenerife</span>
+                    <br><b>Cases 2017</b>
+                    <br><span class="val">10</span>
+                </div>
+            </div>
         </div>
         <div class="col-sm-7">
             <cases-table></cases-table>
         </div>
         <div class="col-sm-3">
-            <b-alert :show="true" state="success" dismissible>
+            <b-alert :show="true" state="warning" dismissible>
                   This is an alert
             </b-alert>
             messages,another info
@@ -20,15 +35,35 @@
             Your case was approved and closed
             Your case was payed
 
-            <div class="card">
+            <div class="card card-copy">
                 <div class="card-block">
-                    @ 2017 MyDoctor
+                    @ 2017 MyDoctor <a href="/about">About</a>
+                    <a href="/help">Help</a>
+                    <a href="/privacy">Privacy</a>
+                    <a href="/terms">Terms</a>
                 </div>
             </div>
         </div>
     </div>
 </template>
-<style>
+<style lang="scss">
+    @import "../sass/variables";
+
+    .doctor-info {
+        margin-top: $offset-doctor-info;
+        margin-bottom: $offset-bottom;
+    }
+
+    .card-copy,
+    .card-about {
+        font-size: $font-size-sm;
+    }
+
+    .card-about {
+        .val {
+            padding-left: .5rem;
+        }
+    }
 </style>
 <script>
 import CasesTable from './cases/CasesTable'
