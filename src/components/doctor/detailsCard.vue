32c3bc4f5517226ccc96cb2a14cfc1271696576f
MedCenter24 Dev <dev@medcenter24.com>
2017-03-26 00:46:19 +0300

    added doctor components

--- src/components/doctor/detailsCard.vue
+++ src/components/doctor/detailsCard.vue
@@ -1,14 +1,16 @@
 <template>
-    <div class="card card-about">
-        <div class="card-block">
-            <h6>About
-                <small> · <a>Edit</a></small>
-            </h6>
-            <b>City</b>
-            <br><span class="val">Tenerife</span>
-            <br><b>Cases 2017</b>
-            <br><span class="val">10</span>
+    <div>
+        <div v-if="doctor" class="card card-about">
+            <div class="card-block">
+                <h6>About
+                    <small> · <a>Edit</a></small>
+                </h6>
+                <b>City</b>
+                <br><span class="ml-1">{{ doctor.city }}</span>
+            </div>
         </div>
+
+        <feedback ref="feedback"></feedback>
     </div>
 </template>
 <style lang="scss">
@@ -21,9 +23,46 @@
     }
 </style>
 <script>
+  import DoctorProvider from '../../providers/doctor.vue'
+  import Feedback from '../../components/ui/dialog/feedback.vue'
+
   export default {
+    components: {
+      Feedback
+    },
+    created: function () {
+      this.fetchData()
+    },
     data () {
-      return {}
+      return {
+        doctor: null
+      }
+    },
+    methods: {
+      fetchData () {
+        this.doctor = null
+        DoctorProvider.get().then(
+          (response) => {
+            this.doctor = response.body
+          },
+          (err) => {
+            let title
+            let text
+            if (err.status === 401) {
+              title = 'Authorization'
+              text = 'You can\'t load list while you are not authorized.'
+            } else if (err.status === 0) {
+              title = 'Request Error'
+              text = 'Not a CORS response'
+            } else {
+              title = 'Request Error'
+              text = '"' + err.status + '" ' + err.statusText
+            }
+
+            this.$refs.feedback.show(title, text)
+          }
+        )
+      }
     }
   }
 </script>
--- src/components/doctor/infoCard.vue
+++ src/components/doctor/infoCard.vue
@@ -1,21 +1,25 @@
 <template>
-    <div v-if="doctor" class="card doctor-info">
-        <img class="card-img-top"
-             :src="doctor.picture_url"
-             height="100px"
-             alt="doctor.name">
-        <div class="card-block">
-            <h6>{{ doctor.name }}</h6>
+    <div>
+        <div v-if="doctor" class="card doctor-info">
+            <img class="card-img-top"
+                 :src="doctor.picture_url"
+                 height="100px"
+                 alt="doctor.name">
+            <div class="card-block">
+                <h6>{{ doctor.name }}</h6>
+            </div>
         </div>
+        <feedback ref="feedback"></feedback>
     </div>
 </template>
-<style lang="scss">
-    @import "../../sass/variables";
-</style>
 <script>
   import DoctorProvider from '../../providers/doctor.vue'
+  import Feedback from '../../components/ui/dialog/feedback.vue'
 
   export default {
+    components: {
+      Feedback
+    },
     created: function () {
       this.fetchData()
     },
@@ -32,24 +36,22 @@
             this.doctor = response.body
           },
           (err) => {
-            this.error = err.toString()
+            let title
+            let text
             if (err.status === 401) {
-              this.errorModal.title = 'Authorization'
-              this.errorModal.bodyText = 'You can\'t load list while you are not authorized.'
+              title = 'Authorization'
+              text = 'You can\'t load list while you are not authorized.'
             } else if (err.status === 0) {
-              this.errorModal.title = 'Request Error'
-              this.errorModal.bodyText = 'Not a CORS response'
+              title = 'Request Error'
+              text = 'Not a CORS response'
             } else {
-              this.errorModal.title = 'Request Error'
-              this.errorModal.bodyText = '"' + err.status + '" ' + err.statusText
+              title = 'Request Error'
+              text = '"' + err.status + '" ' + err.statusText
             }
 
-            this.$refs.errorModal.show()
+            this.$refs.feedback.show(title, text)
           }
         )
-      },
-      onErrorModalClose () {
-        this.$refs.errorModal.hide()
       }
     }
   }
