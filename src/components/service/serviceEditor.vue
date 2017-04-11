1c435d58dd37f01a69df7483f7d51eb8e5f4ce55
zagovorichev <zagovorichev@gmail.com>
2017-04-11 19:44:40 +0300

    created ServiceEditor component

---
+++ src/components/service/serviceEditor.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <button class="btn btn-sm btn-info" @click.prevent>+ {{ $t('Add new service') }}</button>
+    </div>
+</template>
+<style lang="scss">
+</style>
+<script>
+  // import ServiceModel from '../../models/service.vue'
+
+  export default {
+    components: {},
+    data () {
+      return {
+        service: null
+      }
+    },
+    methods: {
+      onSave () {
+        this.$events.fire('service-editor:save', this.service, event)
+      }
+    }
+  }
+</script>
