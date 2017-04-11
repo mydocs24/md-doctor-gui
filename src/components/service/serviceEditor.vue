1bb7a562d06c51564d47f8967135c2d2a14a8b19
zagovorichev <zagovorichev@gmail.com>
2017-04-11 22:59:10 +0300

    adding new service

--- src/components/service/serviceEditor.vue
+++ src/components/service/serviceEditor.vue
@@ -1,6 +1,36 @@
 <template>
     <div>
-        <button class="btn btn-sm btn-info" @click.prevent>+ {{ $t('Add new service') }}</button>
+        <button class="btn btn-sm btn-info" @click.prevent="onOpen">+ {{ $t('Add new service') }}</button>
+
+        <b-modal ref="serviceEditorModal">
+            <h4 slot="modal-header">{{ $t('New service') }}</h4>
+            <div slot="modal-body">
+                <div class="form">
+                    <div class="form-group">
+                        <div class="col-12">
+                            <label class="label">{{ $t('Title') }}</label>
+                            <input type="text" class="form-control" :placeholder="$t('Service title')">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="col-12">
+                            <label class="label">{{ $t('Description') }}</label>
+                            <input type="text" class="form-control" :placeholder="$t('Service description')">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="col-12">
+                            <label class="label">{{ $t('Price') }}</label>
+                            <input type="number" class="form-control" :placeholder="$t('Price')">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <span slot="modal-footer">
+                <button class="btn btn-primary" @click="onSave()">{{ $t('Save') }}</button>
+                <button class="btn btn-secondary" @click="onClose()">{{ $t('Cancel') }}</button>
+            </span>
+        </b-modal>
     </div>
 </template>
 <style lang="scss">
@@ -16,6 +46,12 @@
       }
     },
     methods: {
+      onClose () {
+        this.$refs.serviceEditorModal.hide()
+      },
+      onOpen () {
+        this.$refs.serviceEditorModal.show()
+      },
       onSave () {
         this.$events.fire('service-editor:save', this.service, event)
       }
